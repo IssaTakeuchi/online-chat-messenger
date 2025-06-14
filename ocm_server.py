cfef15c5220433f4eed4f4089ba682daa9a70f3f
IssaTakeuchi <issa.tk.0330@gmail.com>
2025-06-14 11:24:02 +0900

    Update ocm_server.py
    UDPの仕様に変更

--- ocm_server.py
+++ ocm_server.py
@@ -2,44 +2,45 @@ import socket
 import os
 from pathlib import Path
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_address = '0.0.0.0'
 server_port = 9001
-
-# フォルダが存在するかチェックする。存在しない場合、フォルダが作成される。
-# クライアントから受信したファイルを格納するのに必要。
-dpath = 'temp'
-if not os.path.exists(dpath):
-    os.makedirs(dpath)
 
 print('Starting up on {} port {}'.format(server_address,server_port))
 
 # ソケットをサーバーのアドレスをポートに紐付けする
 sock.bind((server_address,server_port))
 
-sock.listen(1)
+try:
+    while True:
+        try:
+            # クライアントから受信したヘッダーを読み取る
+            header,addr = sock.recvfrom(8)
 
-while True:
-    connection,client_address = sock.accept()
-    try:
-        print('connection from',client_address)
-        # クライアントから受信したヘッダーを読み取る
-        header = connection.recv(8)
+            username_length = int.from_bytes(header[:1],"big")
+            data_length = int.from_bytes(header[1:8],"big")
+            stream_rate = 4096
 
-        username_length = int.from_bytes(header[:1],"big")
-        data_length = int.from_bytes(header[1:8],"big")
-        stream_rate = 4096
+            print('Received header from client. Byte lengths: Title length {}, data length {}'.format(username_length,data_length))
 
-        print('Received header from client. Byte lengths: Title length {}, data length {}'.format(username_length,data_length))
+            username_data,_ = sock.recvfrom(username_length)
+            username = username_data.decode('utf-8')
+            print('Username: {}'.format(username))
 
-        username = connection.recv(username_length).decode('utf-8')
-        print('Username: {}'.format(username))
+            if data_length == 0:
+                raise Exception('No data to raead from client.')
+            else:
+                message_data,_= sock.recvfrom(data_length)
+                message = message_data.decode('utf-8')
+                print('Message: {}'.format(message))
 
-        if data_length == 0:
-            raise Exception('No data to raead from client.')
-        else:
-            print('Message: {}'.format(data_length))
+            # データを送信
+            confirmation_massage = f"Server received your massage: '{message}' from '{username}'"
+            sock.sendto(confirmation_massage.encode('utf-8'),addr)
 
-    finally:
-        print("Closing current connection")
-        connection.close()
+        except Exception as e :
+            print(f"Error: {e}")
+
+finally:
+    print("Closing current connection")
+    sock.close()
